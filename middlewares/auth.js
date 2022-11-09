dadfa6eda838761334dfa1767471a71cd24a8784
Aliaksei Levashenia <mrfreeze987@gmail.com>
2022-11-09 23:22:16 +0300

    fix: Токен авторизации

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken');
+const { NOT_FOUND_ERROR_CODE } = require('../utils/constants');
 const ForbiddenError = require('../utils/errors/forbiddenError');
 
 module.exports = (req, res, next) => {
   let payload;
   try {
-    const { authorization } = req.headers;
-    const token = authorization.split(' ')[1];
+    const token = req.headers.authorization.split(' ')[1];
     if (!token) {
       return next(new ForbiddenError('Необходима авторизация'));
     }
-    payload = jwt.verify(token, 'secret-key');
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (e) {
     return next(new ForbiddenError('Необходима авторизация'));
   }
